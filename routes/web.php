b924ea43d33e9d4eebe43b84cf1bd994098a642e
Scott Sutherland <scott.sutherland@we-shape.co.uk>
2021-04-22 00:13:23 +0100

    Development Updates

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/about', function () use ($router) {
+	return "About the API";
+});
+
+$router->get('/job/{id}', function ($id) {
+	return "Job: " . $id;
+});
